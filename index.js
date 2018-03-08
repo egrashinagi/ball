757d1192e9ee3adc5cdfe531525222e3bff15227
egrashina.gi <10Fev1989Gal>
2018-03-08 18:12:53 +0300

    Исправлены стили, организован код

--- index.js
+++ index.js
@@ -1,29 +1,40 @@
 const field = document.querySelector('.field');
 const ball = field.querySelector('.ball');
 
-console.log('Это', field);
-console.log('А это', ball);
+const coordsField = field.getBoundingClientRect();
+const coordsInner = {
+    top: coordsField.top + field.clientTop,
+    left: coordsField.left + field.clientLeft
+};
 
-function searchClick(event) {
-    const coordsField = this.getBoundingClientRect();
-    const coordsInner = {
-        top: coordsField.top + field.clientTop,
-        left: coordsField.left + field.clientLeft
-    };
+function moveBall(x,y) {
+    ball.style.left = x + 'px';
+    ball.style.top = y + 'px';
+}
+
+// move ball to center
+moveBall(200 - ball.clientHeight/2, 100 - ball.clientWidth/2);
+
+function onFieldClick(event) {
     const coordsBall = {
-        top: event.clientY - coordsInner.top - ball.clientHeight / 2,
-        left: event.clientX - coordsInner.left - ball.clientWidth / 2
+        top: event.clientY - coordsInner.top - ball.clientHeight/2,
+        left: event.clientX - coordsInner.left - ball.clientWidth/2
     };
+
     if (coordsBall.top < 0) coordsBall.top = 0;
+
     if (coordsBall.left < 0) coordsBall.left = 0;
+
     if (coordsBall.left + ball.clientWidth > field.clientWidth) {
         coordsBall.left = field.clientWidth - ball.clientWidth;
     }
+
     if (coordsBall.top + ball.clientHeight > field.clientHeight) {
         coordsBall.top = field.clientHeight - ball.clientHeight;
     }
-    ball.style.left = coordsBall.left + 'px';
-    ball.style.top = coordsBall.top + 'px';
+
+    // move ball to coordinates - coordsBall
+    moveBall(coordsBall.left, coordsBall.top);
 }
 
-field.addEventListener('click', searchClick);
+field.addEventListener('click', onFieldClick);
